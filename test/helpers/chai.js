df7bccbc502f44ad8ae76d18becabcc7870fa635
Kelly Selden <kellyselden@gmail.com>
2020-01-18 20:51:33 +0000

    add test

--- test/helpers/chai.js
+++ test/helpers/chai.js
@@ -2,4 +2,6 @@
 
 const chai = require('chai');
 
+chai.use(require('chai-fs'));
+
 module.exports = chai;
---
+++ test/index-test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { describe } = require('./helpers/mocha');
+const { expect } = require('./helpers/chai');
+const path = require('path');
+const {
+  emberInit: _emberInit,
+  prepareBlueprint
+} = require('ember-cli-update-test-helpers');
+
+const fixturesPath = path.resolve(__dirname, 'fixtures');
+
+async function emberInit({
+  args = [],
+  overwrite
+}) {
+  return await _emberInit({
+    args: [
+      '-sn',
+      ...args
+    ],
+    overwrite
+  });
+}
+
+describe(function() {
+  before(async function() {
+    let {
+      blueprintPath,
+      cleanUp
+    } = await prepareBlueprint();
+
+    this.blueprintPath = blueprintPath;
+    this.cleanUp = cleanUp;
+  });
+
+  after(async function() {
+    await this.cleanUp();
+  });
+
+  it('works', async function() {
+    let cwd = await emberInit({
+      args: [
+        '-b',
+        this.blueprintPath
+      ]
+    });
+
+    expect(cwd).to.be.a.directory().and.equal(fixturesPath);
+  });
+});
